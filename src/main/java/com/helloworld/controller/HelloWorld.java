d3fca646df60f7bd38bd32a7faf5bf526f7fa61e
Eyeris Rodriguez Rueda <eyrueda@gmail.com>
2021-05-02 17:27:48 -0500

    feat:add get params

--- src/main/java/com/helloworld/controller/HelloWorld.java
+++ src/main/java/com/helloworld/controller/HelloWorld.java
@@ -2,13 +2,19 @@ package com.helloworld.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api")
 public class HelloWorld {
     @GetMapping(value = "/helloworld")
-    public String sayHelloWorld(){
+    public String sayHelloWorld() {
         return "Hello World!!!";
+    }
+
+    @GetMapping(value = "/hello")
+    public String sayHello(@RequestParam(name = "text") String param) {
+        return param + "Hello!!!";
     }
 }
